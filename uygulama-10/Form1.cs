d7b6cc96b530486a4de57d2e024dbf43dab55388
ırmak akpınar <145127697+irmakakpnr62@users.noreply.github.com>
2023-10-06 16:30:13 +0300

    Add files via upload

---
+++ uygulama-10/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama_10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            { pictureBox1.Visible = radioButton1.Checked; }
+            { pictureBox2.Visible = radioButton2.Checked; }
+            { pictureBox3.Visible = radioButton3.Checked; }
+            { pictureBox4.Visible = radioButton4.Checked; }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            { pictureBox1.Visible = radioButton1.Checked; }
+            { pictureBox2.Visible = radioButton2.Checked; }
+            { pictureBox3.Visible = radioButton3.Checked; }
+            { pictureBox4.Visible = radioButton4.Checked; }
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            { pictureBox1.Visible = radioButton1.Checked; }
+            { pictureBox2.Visible = radioButton2.Checked; }
+            { pictureBox3.Visible = radioButton3.Checked; }
+            { pictureBox4.Visible = radioButton4.Checked; }
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+            { pictureBox1.Visible = radioButton1.Checked; }
+            { pictureBox2.Visible = radioButton2.Checked; }
+            { pictureBox3.Visible = radioButton3.Checked; }
+            { pictureBox4.Visible = radioButton4.Checked; }
+        }
+    }
+}
